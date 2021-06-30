a25afbf51367be050dc71bfeb75693a61ec9465b
fengzhenghui <fengzhenghui@cmss.chinamobile.com>
2021-06-30 15:19:02 +0800

    代码提交

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-view></router-view>
   </div>
-  <router-view />
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { ConfigProvider } from "ant-design-vue";
+export default defineComponent({
+  name: "App",
+  components: {
+    ConfigProvider
+  }
+})
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class='dashboard'>面板</div>
+</template>
+
+<script type='text/ecmascript-6'>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Dashboard',
+  components: {}
+})
+</script>
